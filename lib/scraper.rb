21acabc39ce04dc4c9d307d928feaf51fab1433a
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-24 04:22:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,7 +35,6 @@ class Scraper
         students_detail[:linkedin] = social_media.attribute("href").value
       elsif social_media.attribute("href").value.include?("blog")
         students_detail[:blog] = social_media.attribute("href").value
-        binding.pry
       end
 
 
